985e6aa6b0e05eb234cc8f5c7c9edf119e9f0631
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-08-30 14:58:33 +0530

    created chat page

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,7 +1,17 @@
-const ChatsPage=()=>{
-    return <>
-        chats...
-    </>
-}
+import {PrettyChatWindow} from "react-chat-engine-pretty"
+
+
+const ChatsPage=(props)=>{
+    return (
+    <div style={{height:'100vh'}}>
+         <PrettyChatWindow
+         projectId='1f9017c5-43eb-4fca-b726-8903f3c2541e'
+         username={props.user.username}
+         secret={props.user.secret}
+         style={{height:'100%'}}
+         />
+    </div>
+    )
+};
 
 export default ChatsPage;
